dc871d226fe8e0ec068f9437cf4d32cc83d31285
abrilxcx <abrlbjrn@gmail.com>
2023-04-23 23:32:08 -0600

    131. Manejo errores

--- src/concepts/02-callbacks.js
+++ src/concepts/02-callbacks.js
@@ -7,20 +7,31 @@ import { heroes } from "../data/heroes";
 export const callbacksComponent = ( element ) => {
 
     const id = '5d86371f233c9f2425f16916';
-    findHero( id, (hero) => {
+    findHero( id, (error, hero) => {
+        // element.innerHTML = hero?.name || 'No hay heroe';
+
+        if ( error ) {
+            element.innerHTML = error;
+            return;
+        }
+
         element.innerHTML = hero.name;
     });
 }
 
-
 /**
  * 
  * @param {String} id 
- * @param { (hero: Object) => void } element 
+ * @param { ( error: String|Mull, hero: Object ) => void } callback 
  */
 const  findHero = ( id, callback ) => {
 
     const hero = heroes.find( hero => hero.id === id);
 
-    callback( hero );
+    if ( !hero ) {
+        callback(`Hero with id ${ id } not found.`);
+        return;
+    }
+
+    callback( null, hero );
 }
