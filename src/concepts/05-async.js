d57766309bf9b207b7e201b886ce86d344e6f52c
abrilxcx <abrlbjrn@gmail.com>
2023-07-09 17:00:23 -0600

    139. Manejo errores func async

--- src/concepts/05-async.js
+++ src/concepts/05-async.js
@@ -6,13 +6,14 @@ import { heroes } from "../data/heroes"
  */
 export const asyncComponent = ( element ) => {
 
-    const id1 = '5d86371fd55e2e2a30fe1ccb2';
+    const id1 = '5d86371fd55e2e2a30fe1ccb2-';
+    console.log('Inicio de componente');
 
     findHero( id1 )
         .then( name => element.innerHTML = name )
         .catch( error => element.innerHTML = error)
 
-
+    console.log('Fin del componente');
 }
 
 /**
@@ -23,6 +24,8 @@ export const asyncComponent = ( element ) => {
 const findHero = async( id ) => {
 
     const hero = heroes.find( hero => hero.id === id );
+    if( !hero )
+        throw `Hero with id ${ id } not found`;
 
     return hero.name;
 }
