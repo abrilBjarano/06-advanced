ca57c194191513a56822553e599533c48860e80c
abrilxcx <abrlbjrn@gmail.com>
2023-07-09 13:12:19 -0600

    135. Promise Hell

--- src/concepts/03-promises.js
+++ src/concepts/03-promises.js
@@ -19,10 +19,27 @@ export const promiseComponent = ( element ) => {
         `;
     }
 
+    const renderTwoHeroes = ( hero1, hero2 ) => {
+        element.innerHTML = `
+            <h3>${ hero1.name }</h3>
+            <h3>${ hero2.name }</h3>
+        `;
+    }
+
     const id1 = '5d86371f2343e37870b91ef1';
+    const id2 = '5d86371f25a058e5b1c8a65e';
+
+    let hero1, hero2;
 
     findHero( id1 )
-        .then( renderHero )
+        .then( (hero1) => {
+
+            findHero( id2 )
+                .then( hero2 => {
+                    renderTwoHeroes(hero1, hero2)
+                })
+                .catch( renderError );
+        })
         .catch( renderError );
 
 }
