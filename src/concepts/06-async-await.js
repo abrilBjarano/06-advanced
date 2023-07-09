a06e1a96e48f4e3c524a859a58349cec77f35750
abrilxcx <abrlbjrn@gmail.com>
2023-07-09 17:43:45 -0600

    141. Errores async-await

--- src/concepts/06-async-await.js
+++ src/concepts/06-async-await.js
@@ -6,13 +6,20 @@ import { heroes } from "../data/heroes"
  */
 export const asyncAwaitComponent = async( element ) => {
 
-    const id1 = '5d86371fd55e2e2a30fe1ccb';
+    const id1 = '5d86371fd55e2e2a30fe1ccb-';
     const id2 = '5d86371fd55e2e2a30fe1cc3';
 
-    const hero1 = await findHero( id1 );
-    const hero2 = await findHero( id2 );
+    try {
+        const hero1 = await findHero( id1 );
+        const hero2 = await findHero( id2 );
+        
+        element.innerHTML = ` ${ hero1.name } / ${ hero2.name }`;
 
-    element.innerHTML = ` ${ hero1.name } / ${ hero2.name }`;
+    } catch (error) {
+        element.innerHTML = error;
+    }
+
+
 
 }
 
